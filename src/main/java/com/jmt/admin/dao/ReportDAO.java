6fae4d8d935f2b9f4f66d90f6124f7a91218e89b
Kim inuk <inuk22@naver.com>
2022-08-08 09:20:48 +0900

    2022-08-08 .1

--- src/main/java/com/jmt/admin/dao/ReportDAO.java
+++ src/main/java/com/jmt/admin/dao/ReportDAO.java
@@ -1,5 +1,14 @@
 package com.jmt.admin.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public interface ReportDAO {
+
+	ArrayList<HashMap<String, Object>>  reportList(int cnt, int offset);
+
+	int allCount();
+
+
 
 }
