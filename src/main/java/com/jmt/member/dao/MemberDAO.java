b4953c1e8b8af3a57f97a4da3aee9521edfa4a99
YEONSOYEONG <dusthdud13@naver.com>
2022-08-08 22:19:44 +0900

    암호화 라이브러리 추가 회원가입, 로그인, ID찾기, PW찾기

--- src/main/java/com/jmt/member/dao/MemberDAO.java
+++ src/main/java/com/jmt/member/dao/MemberDAO.java
@@ -1,5 +1,23 @@
 package com.jmt.member.dao;
 
+import java.util.HashMap;
+
 public interface MemberDAO {
+
+	int join(HashMap<String, String> params);
+
+	String idoverlay(String chkId);
+
+	String emailoverlay(String chkEmail);
+
+	String login(String id, String pw);
+
+	String getMbClass(String id, String pw);
+
+	String idFind(String email, String birth);
+
+	String pwFind(String id, String email);
+
+	int pwUpdate(String id, String pw);
 
 }
--- src/main/java/com/jmt/member/dto/MemberDTO.java
+++ src/main/java/com/jmt/member/dto/MemberDTO.java
@@ -1,5 +1,51 @@
 package com.jmt.member.dto;
 
 public class MemberDTO {
+	
+	private String member_id;
+	private String member_pw;
+	private String member_email;
+	private String member_name;
+	private String member_birth;
+	private String member_status;
+	
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getMember_pw() {
+		return member_pw;
+	}
+	public void setMember_pw(String member_pw) {
+		this.member_pw = member_pw;
+	}
+	public String getMember_email() {
+		return member_email;
+	}
+	public void setMember_email(String member_email) {
+		this.member_email = member_email;
+	}
+	public String getMember_name() {
+		return member_name;
+	}
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+	public String getMember_birth() {
+		return member_birth;
+	}
+	public void setMember_birth(String member_birth) {
+		this.member_birth = member_birth;
+	}
+	public String getMember_status() {
+		return member_status;
+	}
+	public void setMember_status(String member_status) {
+		this.member_status = member_status;
+	}
+	
+	
 
 }
