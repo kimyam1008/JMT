d2b0eaac13dd390274384fd79ab9bd01abae0fcb
YEONSOYEONG <dusthdud13@naver.com>
2022-08-09 17:55:03 +0900

    로그인 암호화 부분 수정, 프로필 사진 제외하고 기능 구현

--- src/main/java/com/jmt/member/service/MemberService.java
+++ src/main/java/com/jmt/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.jmt.member.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.slf4j.Logger;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.jmt.member.dao.MemberDAO;
+import com.jmt.member.dto.MemberDTO;
 
 @Service
 public class MemberService {
@@ -25,69 +27,69 @@ public class MemberService {
 		String hashText = "";
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
-		hashText = encoder.encode(params.get("pw"));
+		hashText = encoder.encode(params.get("pw")); //입력받은 pw를 암호화 하기
 		logger.info(hashText);
 		
-		params.put("pw", hashText);
+		params.put("pw", hashText); //암호화 한 pw를 params 에 넣기
 		
-		int row = dao.join(params);
+		int row = dao.join(params); //입력받은 정보로 회원가입 하기
 		boolean success = false;
 		
-		if(row>0) {
+		if(row>0) { // 정보를 DB 에 저장하는 것이 성공하면 success 를 true 로 바꾸기
 			success = true;
 		}
 
-		result.put("success", success);
+		result.put("success", success); // 결과 값 넣기
 		
 		return result;
 	}
 
 	public HashMap<String, Object> idoverlay(String chkId) {
 		HashMap<String, Object> map = new HashMap<String, Object>();
-		String overId = dao.idoverlay(chkId);
+		String overId = dao.idoverlay(chkId); // 입력받은 아이디 DB 에 조회해서 값 가져오기
 		logger.info("중복 아이디가 있나? "+overId);
 		boolean over = overId == null ? false : true; //아이디없음(false) : 아이디있음(true)
-		map.put("idoverlay", over);
+		map.put("idoverlay", over); // 결과 값 넣기
 		
 		return map;
 	}
 
 	public HashMap<String, Object> emailoverlay(String chkEmail) {
 		HashMap<String, Object> map = new HashMap<String, Object>();
-		String overEmail = dao.emailoverlay(chkEmail);
+		String overEmail = dao.emailoverlay(chkEmail); // 입력받은 이메일 DB 에 조회해서 값 가져오기
 		logger.info("중복 이메일이 있나? "+overEmail);
 		boolean over = overEmail == null ? false : true; //이메일없음(false) : 이메일있음(true)
-		map.put("emailoverlay", over);
+		map.put("emailoverlay", over); // 결과 값 넣기
 		
 		return map;
 	}
 
-	public String login(String id, String pw) {
+	public String login(String id, String hashText) {
 		logger.info("로그인 서비스");
-		return dao.login(id,pw);
+		return dao.login(id,hashText);
 	}
 
-	public String getMember_status(String id, String pw) {
+	public String getMember_status(String id, String hashText) {
 		
-		return dao.getMbClass(id,pw);
+		return dao.getMbClass(id,hashText);
 	}
 
 	public ModelAndView idFind(String email, String birth) {
-		String idFind = dao.idFind(email, birth);
+		String idFind = dao.idFind(email, birth); // 이메일과 생년월일로 id 찾기
 		logger.info("아이디 : "+idFind);
-		ModelAndView mav = new ModelAndView();
+		ModelAndView mav = new ModelAndView(); //페이지와 메세지를 동시에 주기 위해 사용
 		String msg = null;
 		String page = null;
-		if(idFind != null) {
+		if(idFind != null) { //조회한 아이디가 존재한다면 경고창으로 알려주고 로그인 페이지로 보내기
 			msg = "회원님의 아이디는 "+"'"+idFind+"'"+" 입니다.";
 			page = "/Member/login";
-		} else {
+		} else { //아이디가 존재하지 않는다면 경고창으로 알려주고 그 페이지 유지하기
 			msg = "존재하지 않는 회원입니다.";
 			page = "/Member/idFind";
 		}
 		
-		mav.addObject("msg",msg);
-		mav.setViewName(page);
+		mav.addObject("msg",msg); // 메세지 넣기
+		mav.setViewName(page); // 페이지 넣기
 		
 		return mav;
 	}
@@ -99,15 +101,41 @@ public class MemberService {
 
 	public HashMap<String, Object> pwUpdate(String id, String pw) {
 		HashMap<String, Object> map = new HashMap<String, Object>();
-		int row = dao.pwUpdate(id, pw);
+		String hashText = "";
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		hashText = encoder.encode(pw); //입력받은 pw를 암호화 하기
+		logger.info(hashText);
+		
+		int row = dao.pwUpdate(id, hashText); //암호화 한 pw 를 DB 에 업데이트 해주기
 		boolean success = false;
 		
-		if(row>0) {
+		if(row>0) { //DB 넣는 것을 성공하면 true 로 바꿔주기
 			success = true;
-			map.put("success", success);
+			map.put("success", success); // 결고 값 넣어주기
 		}
 		
 		return map;
+	}
+
+	public void profileRegister(HashMap<String, Object> params) {
+		dao.profileRegister(params);
+		
+	}
+
+	public String profileExist(String loginId) {
+		
+		return dao.profileExist(loginId);
+	}
+
+	public ArrayList<MemberDTO> foodList() {
+		
+		return dao.foodList();
+	}
+
+	public String con(String id) {
+		
+		return dao.con(id);
 	}
 	
 	
